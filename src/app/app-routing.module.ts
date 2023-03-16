80624e088f95fdcd25f1925c2198374ca2ae866d
abhiDudhe <abhidudhe2000@gmail.com>
2023-03-16 23:08:59 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Blog1Component } from './blog1/blog1.component';
+import { Blog2Component } from './blog2/blog2.component';
+import { Blog3Component } from './blog3/blog3.component';
+import { Blog4Component } from './blog4/blog4.component';
+import { Blog5Component } from './blog5/blog5.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path: "home",
+  component:HomeComponent},
+{  path: "blog1",
+  component:Blog1Component},
+  {  path: "blog2",
+  component:Blog2Component},
+  {  path: "blog3",
+  component:Blog3Component},
+  {  path: "blog4",
+  component:Blog4Component},
+  {  path: "blog5",
+  component:Blog5Component},
+  {  path: "contact",
+  component:ContactUsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Blog1Component } from './blog1/blog1.component';
+import { Blog2Component } from './blog2/blog2.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { Blog3Component } from './blog3/blog3.component';
+import { Blog4Component } from './blog4/blog4.component';
+import { Blog5Component } from './blog5/blog5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Blog1Component,
+    Blog2Component,
+    HomeComponent,
+    ContactUsComponent,
+    Blog3Component,
+    Blog4Component,
+    Blog5Component
   ],
   imports: [
     BrowserModule,
